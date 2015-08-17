f66be80aa2aedddf20d62cdaeb613cf26037ff11
Sergey Kvachonok <ravenexp@gmail.com>
2015-08-17 10:12:11 +0300

    Add AGPLv3 legalese boilerplate to the source files.
    Use the shortened version because nobody really cares
    about warranties and liabilities.

--- src/nsafepass.c
+++ src/nsafepass.c
@@ -1,3 +1,11 @@
+/*  This file is part of nsafepass
+    Copyright 2015 Sergey Kvachonok
+
+    nsafepass is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version. */
+
 #include <stdio.h>
 #include <config.h>
 
